3ee72ccea2db9c8b00ca1aaf4cd8460407aa22b1
Bhargavi Padhya <padhya.b@husky.neu.edu>
2022-05-23 21:34:42 +0530

    Cover photo and intro changes

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Home.scss';
 import home1 from './assets/FullSizeRender.jpg';
+import test from './assets/DSC_0434.JPG'
+import trekcover from './assets/trekcover.jpg';
+//import trekcover from './assets/IMG_0358.jpg';
+import headshot from './assets/Headshot.jpg';
 import Navbar from "./Navbar";
 import {recommendationList} from './recommendationList';
 import Footer from "./Footer";
@@ -9,13 +13,14 @@ const Home = () => {
     return (
         <div className="Home">
             <Navbar/>
-            <img src={home1} className="home-image" alt="picture of bhargavi padhya taking in the sun & surrounded by nature"/>
+            <img src={trekcover} className="home-image" alt="picture of bhargavi padhya taking in the sun & surrounded by nature"/>
             <div className="intro-section">
+                <img src={headshot} className="headshot"/>
                 <h1 className="name-heading">
                     Bhargavi Padhya
                 </h1>
                 <div className="name-subheading">
-                    Designer, Developer, Bookworm, Fitness enthusiast
+                    Designer, Developer, Bookworm, Trekker
                 </div>
 
             </div>
@@ -24,7 +29,7 @@ const Home = () => {
                 <p>I am a developer who brings concepts and designs to life using code. I'm also a designer who makes sure that, that code, results in providing a delightful user experience.
                 I'm a UX unicorn in the world full of designers and developers. I love having the knowledge from both the worlds, and applying those to solve complex UI problems.
                     I'm a curious life-long learner, a motivated self-starter and a result-oriented person. I love working in teams, there's just something about that buzz of working together towards a shared common goal. When it comes to work, I would like to be involved in projects that I find challenging and with people that inspire me.</p>
-                <p>If you catch me outside of work, I'm an avid crime novel reader, tennis lover, and you'll most likely see me petting dogs and cats.</p>
+                <p>If you catch me outside of work, I'm an avid crime novel reader, tennis lover, and you'll most likely see me petting dogs and cats. If you want to spend time with me, come trek with me, because that's where you'll find me!</p>
             </div>
             <div className="kind-words">
                 <h1 className="name-heading">Kind words</h1>
